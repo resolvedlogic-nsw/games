75ce4dc0f3d4b06bf43306c357c8a7c203780e78
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-05-03 17:22:52 +1000

    Update urls.py

--- master_config/urls.py
+++ master_config/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 
     # 2. Add your games here as separate 'rooms'
     path('knightstrap/', include('knightstrap.urls')),
-    path('cinqo/', include('cinqo.urls')),
-    path('fivecrowns/', include('fivecrowns.urls')),
-    path('labyrinth_game/', include('labyrinth_game.urls')),
+    #path('cinqo/', include('cinqo.urls')),
+    #path('fivecrowns/', include('fivecrowns.urls')),
+    #path('labyrinth_game/', include('labyrinth_game.urls')),
      
 ]
