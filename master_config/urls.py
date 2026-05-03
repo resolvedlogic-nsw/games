be11114deea183f0cb5a8fd33953ea56fa62bc91
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-05-03 17:00:56 +1000

    fix gamehub

--- master_config/urls.py
+++ master_config/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('gamehub.urls')),             # The Hub owns the front door
-    path('knightstrap/', include('knightstrap.urls')), # Game 1
-    path('cinqo/', include('cinqo.urls')),             # Game 2
+    # 1. The Hub owns the homepage
+    path('', hub_views.hub_home, name='home'),
+
+    # 2. Add your games here as separate 'rooms'
+    path('knightstrap/', include('knightstrap.urls')),
+    path('cinqo/', include('cinqo.urls')),
+    path('fivecrowns/', include('fivecrowns.urls')),
+    path('labyrinth/', include('labyrinth_game.urls')),
+     
 ]
