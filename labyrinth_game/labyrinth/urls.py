0cd94393c5f5ebf8be484b03e210a0614ef1bfc4
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-05-03 16:45:59 +1000

    add gamehub

--- labyrinth_game/labyrinth/urls.py
+++ labyrinth_game/labyrinth/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from gamehub import views as hub_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
+    path('', hub_views.index, name='index'), # This makes the Hub the home page
+    path('labyrinth/', include('labyrinth_app.urls')), # Moves the game to a sub-folder
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
