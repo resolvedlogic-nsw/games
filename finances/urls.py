ac6bdfe299f9c482c531272efefa856d753d11c6
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-06-29 20:42:57 +1000

    Update urls.py

--- finances/urls.py
+++ finances/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='finances-home'),
+    path('run/', views.run_report, name='finances-run'),
     path('download/', views.download_csv, name='finances-download'),
 ]
