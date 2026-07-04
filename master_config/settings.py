eb22c0c5f04f8455357d23587f42ab3171044826
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-07-04 21:51:15 +1000

    Update settings.py

--- master_config/settings.py
+++ master_config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-labyrinth-game-dev-key-change-in-production'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
